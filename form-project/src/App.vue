481a30858d3bc43cd62b510a2da0fba3cda5c676
hugo <hugo.alonso.7e5@itb.cat>
2024-10-22 18:27:02 +0200

    Se pasa el nombre bien bien

--- form-project/src/App.vue
+++ form-project/src/App.vue
@@ -1,53 +1,20 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+  import User_component from "./components/User_component.vue"
+
+  function recibir(user){
+    console.log(user.nom.value)
+  }
+  
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="./assets/logo.svg"
-      width="125"
-      height="125"
-    />
+  <div>
+    <h1>Form épico</h1>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <User_component @enviar="recibir"/>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
---
+++ form-project/src/components/User_component.vue
@@ -0,0 +1,33 @@
+<script setup>
+  import { ref } from 'vue';
+  let nomUsuario = ref("");
+  let numUsuario = ref("");
+
+  const emit = defineEmits(['enviar'])
+
+  let user = {
+    nom:String,
+    num:Number
+  }
+
+  function enviar(){
+    user.nom = nomUsuario
+    user.num = numUsuario
+    emit('enviar',user)
+  }
+
+</script>
+
+<template>
+  <div>
+
+    <input type="text" placeholder="Nombre de usuario" v-model="nomUsuario">
+    <input type="number" placeholder="Numero de telefono" v-model="numUsuario">
+    <button @click="enviar">logearse</button>
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
